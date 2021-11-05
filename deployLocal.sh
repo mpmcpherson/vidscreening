598f18482ed8e3f2e393073370d0975b84c49b0a
Michael McPherson <michael@mpmcpherson.com>
2021-11-05 01:33:26 -0400

    init

---
+++ deployLocal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo cp -r /home/michaelmcpherson/src/vidscreening /var/www/html
---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<body style="background-color: black;">
+<video src='videos/juno.mp4' width='100%' controls></video>
+
+
+</body>
+</html>
